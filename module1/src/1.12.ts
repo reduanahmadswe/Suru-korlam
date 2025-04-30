d937745d7d3ca87d9c3707ca9f9879c452a37567
Reduan Ahmad <reduanahmad363737@gmail.com>
2025-05-01 00:56:34 +0600

    Implement nullable types and error handling in search and speed conversion functions

---
+++ module1/src/1.12.ts
@@ -0,0 +1,42 @@
+{
+    //nullabel types
+
+    const searchName = (value : string)=>{
+        if(value ){
+            console.log("Searching for", value);
+        }else{
+            console.log("There is nothing to search");
+        }
+    };
+
+    searchName("Reduan");
+    searchName(null);
+
+    const getSpeedInMeterperSecond = (value:unknown)=>{
+        if(typeof value ==='number'){
+            const convertedSpeed = (value * 1000)/3600;
+            console.log("Speed in meter per second", convertedSpeed);
+        }
+        else if(typeof value === 'string'){
+            const [result,unit] = value.split(" ");
+            console.log(result);
+            const convertedSpeed = (parseFloat(result) * 1000)/3600;
+            console.log("Speed in meter per second", convertedSpeed);
+        }else{
+            console.log("Invalid speed value");
+
+        }
+
+    };
+
+    getSpeedInMeterperSecond(`1000 kmh^-1`);
+
+    
+    // error: Argument of type 'null' is not assignable to parameter of type 'string'.
+
+    const  throwError = (msg:string):never =>{
+        throw new Error(msg);
+    }
+
+    throwError("This is an error message");
+}
