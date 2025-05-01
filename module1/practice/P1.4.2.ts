910b25abdd0c94b0d6c6492ab4941f58c0962022
Reduan Ahmad <reduanahmad363737@gmail.com>
2025-05-01 17:10:00 +0600

    Refactor and expand TypeScript practice files with variable declarations, array manipulations, and tuple usage

---
+++ module1/practice/P1.4.2.ts
@@ -0,0 +1,59 @@
+{
+    
+
+
+    // let name:string = 'Redua Ahmad';
+    // console.log(name);
+
+    // let age:number = 43;
+    // console.log(age);
+
+    // let playerName:string;;
+    // console.log(playerName);//error: Variable 'playerName' is used before being assigned.
+    // playerName = 'Redua Ahmad';
+    // console.log(playerName);
+
+    // playerName = 8954; //error: Type 'number' is not assignable to type 'string'.
+    // console.log(playerName);
+
+    // let isStudent:boolean = true;
+
+    // console.log(isStudent);
+
+    // let x : undefined = undefined;
+
+    // let y : null = null;
+
+    // let d : number = 83945;
+    
+    // array 
+    let friends : string[]= ['rahman','abid','sami','rahat'];
+
+    let eligibleRollList : number []= [123,124,125,126,127];
+
+    eligibleRollList.push(128);
+    eligibleRollList.push(129);
+    eligibleRollList.push(130);
+
+    friends.push('Reduan');
+    friends.push('Rifat');
+    friends.push('Rafiq');
+    friends.push('Rahat');
+
+
+    let coordinatesn : [number,number ]= [1,5];
+    let person : [name:string,age:number,phone:number,address:string] = ['Reduan',23,1234567890,'Dhaka'];
+
+    let mix : (string | number | boolean)[] = ['apple', 42, true, 'banana', 3.14, false, 100];
+    mix.push('orange');
+    mix.push(200);
+
+    mix.push(true);
+    mix.push('grape');
+    mix.push(3.14);
+
+    console.log(mix);
+
+
+
+}
