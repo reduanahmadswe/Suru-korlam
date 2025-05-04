a1008f211f0d7fa957bb603eb1adcf0ceb7e6273
Reduan Ahmad <reduanahmad363737@gmail.com>
2025-05-05 01:45:37 +0600

    function with generics

--- module2/2.5.ts
+++ module2/2.5.ts
@@ -2,26 +2,86 @@
 
     //function with generics
 
-    const createArray = (param: string): string[] => {
-        return [param]
-    }
-    const res1 = createArray('Bangladesh')
-
-    const createArrayWithGeneric = <T>(param: T): T[] => {
-        return [param]
+    const createArray = (pharm: string): string[] => {
+        return [pharm]
     }
 
+
+    // const createArrayWithGeneric = (pharm : string) : string[] => {
+    //     return [pharm]
+    // }
+    const createArrayWithGeneric = <T>(pharm: T): T[] => {
+        return [pharm]
+    }
+
+    const res1 = createArray('Bangladesh');
     const resGeneric = createArrayWithGeneric<string>('Bangladesh')
 
     interface User {
-        id: number,
-        name: string
-    };
+        id: number;
+        name: string;
+    }
+    // const resGenericObj = createArrayWithGeneric<{id:number;name:string}>({
+    //     id : 333,
+    //     name : 'Mr. Reduan',
+    // })
+    const resGenericObj = createArrayWithGeneric<User>({
+        id: 333,
+        name: 'Mr. Reduan',
+    })
 
-    const resGenericObj = createArrayWithGeneric<User>(
+
+
+    const createArrayWithTuple = <T, Q>(parm1: T, parm2: Q): [T, Q] => {
+        return [parm1, parm2]
+    }
+
+    const res10 = createArrayWithTuple('Bangladesh', 87345);
+    const resGeneric10 = createArrayWithTuple<string, { name: string }>('Bangladesh', { name: 'Asia' })
+
+    interface User {
+        id: number;
+        name: string;
+    }
+    // const resGenericObj = createArrayWithGeneric<{id:number;name:string}>({
+    //     id : 333,
+    //     name : 'Mr. Reduan',
+    // })
+    const resGenericObj10 = createArrayWithTuple<User, { presentAdd: string }>(
         {
-            id: 222,
-            name: 'reduan'
+            id: 333,
+            name: 'Mr. Reduan',
+        },
+        {
+            presentAdd : 'sldkfj',
+        }
+        
+    )
+
+
+    const addCourseToStudent = <T> (student : T)=>{
+        const course = 'Next level web Development'
+
+        return {
+            ...student,
+            course
+        }
+    }
+
+    const student1 = addCourseToStudent(
+        {
+            name:'Mr x',
+            email :'dksfa@gkadf.com',
+            devType : 'NLWD'
+        }
+    )
+
+    const student2 = addCourseToStudent(
+        {
+            name:'Mr Y',
+            email :'sfa@gkadf.com',
+            devType : 'NLWD',
+            hasWatch : 'Apple Watch'
         }
     )
 
