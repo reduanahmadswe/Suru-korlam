5f8ff9394c17f11f4ac66cfaa25c9bfa66edc55f
Reduan Ahmad <reduanahmad363737@gmail.com>
2025-05-06 21:05:26 +0600

    access modifires

---
+++ module3/3.5.ts
@@ -0,0 +1,37 @@
+{
+    //access modifires 
+    class BankAccount {
+        public readonly id: number;
+        public name: string;
+        protected _balance: number;
+
+
+        constructor(id: number, name: string, balance: number) {
+            this.id = id;
+            this.name = name;
+            this._balance = balance;
+        }
+
+        public addDeposit(amount: number) {
+            this._balance = this._balance + amount
+        }
+
+        public getBalance() {
+            return this._balance;
+        }
+
+    }
+
+    class StudentAccount extends BankAccount {
+        test(){
+            this.
+        }
+    }
+
+    const goribManusherAccount = new BankAccount(111, 'Mr.gorib ', 70);
+    goribManusherAccount.addDeposit(20);
+    const myBalance = goribManusherAccount.getBalance();
+
+    console.log(myBalance);
+
+}
